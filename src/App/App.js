b63f3c22d60bb4211e97b3ea3e613b56ba47abe7
Crystal Broach <crysland@gmail.com>
2020-01-20 10:32:34 -0600

    setup complete

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ticket Counter</h1>
+      <button className="btn btn-info">Coolest Button</button>
+    </div>
+  );
+}
+
+export default App;
